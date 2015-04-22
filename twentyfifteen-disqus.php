d3673ce9fe17c044c792cfafc49f8c88d3df352e
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2015-04-21 22:16:12 -0500

    use proper hook

--- twentyfifteen-disqus.php
+++ twentyfifteen-disqus.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action( 'init', 'twentyfifteen_disqus' );
+add_action( 'wp_enqueue_scripts', 'twentyfifteen_disqus' );
 
 function twentyfifteen_disqus() {
   wp_enqueue_style( __FILE__ . '/twentyfifteen-disqus.css' );
