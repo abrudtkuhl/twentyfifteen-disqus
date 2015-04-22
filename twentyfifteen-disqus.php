4b92010f33c345cab998835a943e3f04708fb753
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2015-04-21 22:19:47 -0500

    plugin refactor

--- twentyfifteen-disqus.php
+++ twentyfifteen-disqus.php
@@ -1,7 +1,19 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'twentyfifteen_disqus' );
+/**
+ * Plugin Name: Gravity Forms Bootstrap 3 Style
+ * Plugin URI: https://github.com/abrudtkuhl/WordPress-Bootstrap-Gravity-Forms
+ * Description: Use Bootstrap 3 form styles with Gravity Forms
+ * Version: 0.1
+ * Author: Andy Brudtkuhl
+ * Author URI: http://youmetandy.com
+ */
 
-function twentyfifteen_disqus() {
-  wp_enqueue_style( __FILE__ . '/twentyfifteen-disqus.css' );
+if( ! function_exists( 'twentyfifteen_disqus' ) ) {
+
+  function twentyfifteen_disqus() {
+    wp_enqueue_style( 'twentyfifteen-disqus', plugins_url( 'gravity-forms-bootstrap.css' ), array(), '1.0', 'all' );
+  }
+
+  add_action( 'wp_enqueue_scripts', 'twentyfifteen_disqus' );
 }
